efb4bc82ffa29fbec32e4c93865773a094d9274e
Jason Yapri <jason.yapri@gmail.com>
2021-04-23 22:29:57 +0700

    Initial Commit

---
+++ android/app/src/main/java/com/jasonyapri/hello_timezone/MainActivity.java
@@ -0,0 +1,6 @@
+package com.jasonyapri.hello_timezone;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('My Flutter App!'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text(
+        'Hello World!',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text('click'),
+      backgroundColor: Colors.red[600]
+    ),
+  ),
+));
