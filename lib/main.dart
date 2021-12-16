4ccfe01d403796ccaa7131045d4bc77a83b66fdd
Jason Yapri <jason.yapri@gmail.com>
2021-12-16 16:30:30 +0700

    Widget Lifecycle (#24)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hello_timezone/pages/home.dart';
+import 'package:hello_timezone/pages/loading.dart';
+import 'package:hello_timezone/pages/choose_location.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home()
+  // home: Home(),
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(), // Default Initial Route, except specified
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+}
 ));
 
--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,10 +7,36 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    print('build function ran');
+
     return Scaffold(
-      body: Text('choose location screen')
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: TextButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: Text('counter is $counter'),
+      )
     );
   }
 }
