67f8c02c8ab508fe2fdb87a1593c4a202fb8bacb
Jason Yapri <jason.yapri@gmail.com>
2021-12-17 14:52:23 +0700

    Formatting & Showing Dates (#31)

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
 
@@ -27,7 +28,7 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(offset)));
 
       // set the time property
-      time = now.toString();
+      time = DateFormat.jm().format(now);
       print('getTime ended');
     }
     catch(e) {
